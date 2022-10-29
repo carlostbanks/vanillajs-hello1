2cc715bb064abe5ff69c360d9921815fbb908978
Carlos Banks <carlostbanks@gmail.com>
2022-10-29 00:48:06 +0000

    project complete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+
+  let newNumber = document.querySelector(".number");
+  let newTopSuit = document.querySelector(".top-suit");
+  let newBottomSuit = document.querySelector(".bottom-suit");
+
+  function randomCard(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  function card() {
+    let number = numbers[randomCard(numbers)];
+    let suit = suits[randomCard(suits)];
+    newNumber.innerHTML = number;
+    newTopSuit.innerHTML = suit;
+    newBottomSuit.innerHTML = suit;
+  }
+
+  card();
 };
